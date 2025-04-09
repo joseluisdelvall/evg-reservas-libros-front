f8f4c4bbe16a094b8bf602d9bdec60d7c18cb52e
joseluisdelvall <joseluisdelvall01@gmail.com>
2025-04-09 19:39:50 +0200

    UPDATE: Install toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { CrudComponent } from './components/gestion/crud/crud.component';
 import { AsideComponent } from './components/general/aside/aside.component';
 import { ModalContentComponent } from './components/shared/modal-content/modal-content.component';
 import { ModalNuevoComponent } from './components/gestion/crud/components/modales/modal-nuevo/modal-nuevo.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,12 @@ import { ModalNuevoComponent } from './components/gestion/crud/components/modale
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({ // Configuración global opcional de Toastr
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
