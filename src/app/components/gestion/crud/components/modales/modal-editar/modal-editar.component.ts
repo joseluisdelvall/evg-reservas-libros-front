e599efa460f0ff8b1a2da69bc85965b5eb34d4bb
asanchezEVG <asanchezdiaz.guadalupe@alumnado.fundacionloyola.net>
2025-04-09 20:15:44 +0200

    UPDATE: Cambios de estilo en modales

---
+++ src/app/components/gestion/crud/components/modales/modal-editar/modal-editar.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ModalOptions } from 'src/app/components/shared/modal-content/models/modal-options';
+
+@Component({
+  selector: 'app-modal-editar',
+  templateUrl: './modal-editar.component.html',
+  styleUrls: ['./modal-editar.component.css']
+})
+export class ModalEditarComponent implements OnInit, OnChanges {
+
+  @Input() modo: 'libros' | 'editoriales' | null = null;
+  
+    // editarModalOptions
+    editarModalOptions!: ModalOptions;
+  
+    constructor() { }
+  
+    ngOnInit(): void {}
+  
+    ngOnChanges(changes: SimpleChanges): void {
+      if (changes['modo'] && changes['modo'].currentValue) {
+        this.editarModalOptions = {
+          title: 'Editar ' + (this.modo === 'libros' ? 'libro' : 'editorial'),
+          modalId: 'editar' + this.modo,
+          size: 'xl',
+          okButton: {
+            text: 'Aceptar',
+          },
+          cancelButton: {
+            text: 'Cancelar',
+          },
+        };
+      }
+    }
+  
+}
