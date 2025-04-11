091f2e40535bae947f9972db90ead4bcda15615d
joseluisdelvalle <jl.valle@vitaly.es>
2025-04-11 08:20:39 +0200

    UPDATE: fix prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    baseUrl: ''
+  },
+  auth: {
+    cookieTokenName: 'RDLEVGTKN',
+  },
+  pathImg: 'assets/img/'
 };
