b7930a92886c0156920ff6185e32243f291d327d
Merroji <156344132+merroji@users.noreply.github.com>
2025-04-24 19:20:48 +0200

    Update: Reconstrucción app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormReservaComponent } from './components/form-reserva/form-reserva.component';
-import { LoginComponent } from './admin/components/login/login.component';
-import { ReservasComponent } from './admin/components/reservas/reservas.component';
-import { PedidosComponent } from './admin/components/pedidos/pedidos.component';
-import { CrudComponent } from './admin/components/gestion/crud/crud.component';
-import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   // Default route ESTO HAY QUE CAMBIARLO, HAY QUE HACER OTRO ROUTING DE /ADMIN
   // Rutas "externas"
   { path: 'form-reserva', component: FormReservaComponent},
-  { path: '**', redirectTo: 'form-reserva'},
   // Rutas "privadas"
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'reservas', component: ReservasComponent, canActivate: [AuthGuard] },
-  { path: 'pedidos', component: PedidosComponent, canActivate: [AuthGuard] },
-  { path: 'crud/:modo', component: CrudComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  //{ path: '**', redirectTo: 'login' }
+  { path: '**', redirectTo: 'form-reserva'}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
