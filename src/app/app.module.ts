6c5ad6a6f437c5c770db10fdc6b2d2d34ee484fd
asanchezEVG <asanchezdiaz.guadalupe@alumnado.fundacionloyola.net>
2025-05-05 11:59:21 +0200

    UPDATE: Cambios en el formulario de reserva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ModalPeriodoReservasComponent } from './admin/components/reservas/modal
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
+
 @NgModule({
   declarations: [
     AppComponent,
