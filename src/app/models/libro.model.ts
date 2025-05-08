17293bb6d88669bc615aab3dfd9cd5ad90eccf04
asanchezEVG <asanchezdiaz.guadalupe@alumnado.fundacionloyola.net>
2025-05-08 18:27:47 +0200

    UPDATE: form reserva avanzado

--- src/app/models/libro.model.ts
+++ src/app/models/libro.model.ts
@@ -1,11 +1,10 @@
 import { Editorial } from "./editorial.model";
 
 export interface Libro {
-  id?: number; // Identificador único del libro
-  nombre?: string; // Título del libro
-  isbn?: string; // Autor del libro
-  precio?: number; // Precio del libro
-  stock?: number; // Año de publicación
-  editorial?: Editorial; // Editorial del libro
-  estado?: boolean; // Estado del libro (disponible o no)
+    id: number;
+    nombre: string;
+    isbn: string;
+    editorial: Editorial;
+    estado: boolean;
+    precio: number;
 }
