075cc14f1cfbc8e8323af45d0ef248239b94379c
joseluisdelvall <joseluisdelvall01@gmail.com>
2025-05-12 11:47:31 +0200

    UPDATE: fix front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: true,
   api: {
-    baseUrl: ''
+    baseUrl: 'https://12.2daw.esvirgua.com/evg-reservas-libros-back/api',
+    loginUrl: 'https://12.2daw.esvirgua.com/evg-reservas-libros-back/api/login',
   },
   auth: {
     cookieTokenName: 'RDLEVGTKN',
   },
-  pathImg: 'assets/img/'
+  pathImg: 'assets/img/',
+  googleClientId: '660176374148-klpm52u3brlqsmpjvqci3ruk5qk1ofnl.apps.googleusercontent.com'
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
