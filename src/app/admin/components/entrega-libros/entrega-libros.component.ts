7fffe8bea323cd7c3228730c5b457210b94ea12f
joseluisdelvall <joseluisdelvall01@gmail.com>
2025-05-31 23:15:10 +0200

    UPDATE: componente creado SIN HACER

---
+++ src/app/admin/components/entrega-libros/entrega-libros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entrega-libros',
+  templateUrl: './entrega-libros.component.html',
+  styleUrls: ['./entrega-libros.component.css']
+})
+export class EntregaLibrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
