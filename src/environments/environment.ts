b2721c7553e7ca14ad4fdee88b64ed391adce571
Celia <celiamorunoherrojo@gmail.com>
2025-06-05 03:33:11 +0200

    Fix: Enviroment [Production: true]

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   api: {
     baseUrl: 'http://localhost/evg-reservas-libros-back/api',
     loginUrl: 'http://localhost/evg-reservas-libros-back/api/login',
